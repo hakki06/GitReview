b24f28789616146e3265e757cf5d61e972eda098
sekercih <hkkskrc@gmail.com>
2020-09-29 19:36:33 +0300

    son versiyon

--- src/Ders1.java
+++ src/Ders1.java
@@ -7,10 +7,7 @@ public static void main(String[] args) {
 	
 	System.out.println("Saat 18:46");
 	System.out.println("Saat 19:00");
-	
-	
 	System.out.println("Saat 19:17-hasanbey");
-
 	System.out.println("Saat 19:07-conflict");
 
 }
